e74c40c6269e1e7cd70a26b1e7e21c91579ff023
gwanyong_04 <43106908+YYOngYYong@users.noreply.github.com>
2020-08-05 21:44:15 +0900

    Updates

---
+++ precache-manifest.52b959ed92e1463257b63278b3a51bc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab59dafbd92ac18e8f66a4eb167c5794",
+    "url": "/react_movie/index.html"
+  },
+  {
+    "revision": "d052e446c7af0875b8cc",
+    "url": "/react_movie/static/css/main.4806f1c1.chunk.css"
+  },
+  {
+    "revision": "9fc5cdcbaa33fe1c9afc",
+    "url": "/react_movie/static/js/2.156baf9f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_movie/static/js/2.156baf9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d052e446c7af0875b8cc",
+    "url": "/react_movie/static/js/main.2b579926.chunk.js"
+  },
+  {
+    "revision": "5260ec61c5af55128689",
+    "url": "/react_movie/static/js/runtime-main.f41785c0.js"
+  }
+]);
