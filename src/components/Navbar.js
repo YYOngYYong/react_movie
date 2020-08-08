bbcc657cc8b79adbe9adf80c0db0be92df82b559
gwanyong_04 <43106908+YYOngYYong@users.noreply.github.com>
2020-08-08 16:17:00 +0900

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,17 @@ import "./Navbar.css";
 
 function Navbar() {
   return (
-    <div className="nav">
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-    </div>
+    // <div className="nav">
+    //   <Link to="/">Home</Link>
+    //   <Link to="/about">About</Link>
+    // </div>
+    <nav id="navbar">
+      <div className="navbar__logo">
+        <Link to="/" className="title">
+          Yongflix
+        </Link>
+      </div>
+    </nav>
   );
 }
 
